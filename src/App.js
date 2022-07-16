5eb33634dc297b1baee447e59f0d84223d6ce6fd
maxbirimkulov <maxbirimkulov@mail.ru>
2022-07-17 02:06:59 +0600

    add tset

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import image from './2022-07-17 02.05.58.jpg'
 
 function App() {
-  return <h1>123123123123</h1>;
+  return <div>
+    <h1>Идите нахуй пацаны</h1>
+    <img src={image} alt=""/>
+  </div>;
 }
 
 export default App;
